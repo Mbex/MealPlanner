02f6eca763e2e8092181050d59a805cecdd2edda
Mike <michael.priestley@postgrad.manchester.ac.uk>
2017-02-21 21:55:04 +0000

    improved parsing of ingredients. attempted to make js form delete doesnt work yet

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,76 @@
+console.log("Js loading");
 
-console.log("this is working");
+var LOCAL_HOST = 'http://127.0.0.1:5000/';
+
+function httpGet(theUrl) {
+    return new Promise( function(resolve, reject) {
+        page = new XMLHttpRequest();
+        page.addEventListener('load', function(evt){
+            resolve(JSON.parse(this.responseText));
+        });
+        page.addEventListener('error', function(error){
+            reject(error);
+        });
+        page.open("GET", theUrl);
+        page.send();
+    });
+}
+
+
+
+// populate database list
+httpGet(LOCAL_HOST.concat('meals/')).then( function (all_meals) {
+
+    var dsp_meals = document.getElementById('db-meal-list');
+
+    all_meals.forEach( function(element) {
+
+      var entry = document.createElement('div');
+      var entry_major = document.createElement("p");
+      var entry_minor = document.createElement("p");
+      var entry_del_button = document.createElement('button');
+      var form = document.createElement('form');
+      var input = document.createElement('input');
+
+      entry.setAttribute('id',element._id);
+      entry_major.innerHTML = element.name;
+      entry_minor.innerHTML = JSON.stringify(element.ingredients);
+      entry_del_button.innerHTML = 'Delete';
+
+      // input.setAttribute('id', element._id);
+      // input.style.display = 'hidden';
+      // form.appendChild(input);
+      form.setAttribute('method', 'delete');
+      form.setAttribute('id','form'.concat(element._id))
+      form.style.display = 'hidden';
+      form.setAttribute('action', LOCAL_HOST.concat('meals/keyword=id/value=').concat(element._id));
+      entry.appendChild(form);
+
+      entry_del_button.addEventListener('click', function(evt) {
+
+        var id = this.parentNode.getAttribute('id');
+        var element = document.getElementById(id);
+        element.parentNode.removeChild(element);
+        getElementById(form.concat(id)).submit();
+
+
+      });
+
+      entry.appendChild(entry_major);
+      entry.appendChild(entry_minor);
+      entry.appendChild(entry_del_button);
+      dsp_meals.appendChild(entry);
+
+    }
+
+//  dsp_meals.innerHTML = meal_list.tostring();
+  )
+});
+
+
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//    if (this.readyState == 4 && this.status == 200) {
+//       // Action to be performed when the document is read;
+//    }
+// };
--- src/py/app.py
+++ src/py/app.py
@@ -1,22 +1,24 @@
-import json
-import flask
-from flask_cors import CORS
 from MealPlanner.MealPlan import MealPlan
 from MealPlanner.Meal import Meal
+from MealPlanner.databaseCRUDService import mongoCRUD
 import socket
+import flask
+from flask_cors import CORS
+import re
 
 
 # --------for on the pi------------
 # also app.run at the bottom
-#----------------------------------
 #ipaddr = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
-
+#----------------------------------
 
 app = flask.Flask(__name__)
 CORS(app)
-instance = MealPlan('MealPlan')
+meal_db = mongoCRUD('meals')
 
-#------------ Views ------------------------------------#
+
+#mealplan_db = MealPlan('MealPlans')
+#------------- Views ------------------------------------#
 # @app.route('/', methods = ['GET'])
 # def home():
 #
@@ -30,7 +32,8 @@ instance = MealPlan('MealPlan')
 #     '''interact with database.'''
 #
 #     return flask.send_from_directory(HTML_DIR,'database.html')
-#------------- databaseCRUDService Methods -------------#
+
+#------------- databaseCRUDService Meal Methods -------------#
 @app.route('/meals/', methods = ['GET', 'POST', 'DELETE'])
 def AllEntries():
 
@@ -39,62 +42,103 @@ def AllEntries():
     if flask.request.method == "GET":
 
         #List entries based on keyword passed.
-        return flask.jsonify(instance.readAll())
+        return flask.jsonify(meal_db.readAll())
 
     if flask.request.method == 'POST':
 
-        #Create new entry.
+        '''Create new entry.'''
         name = flask.request.form['name']
         meal_type = flask.request.form['meal type']
-        ingredients = flask.request.form['ingredient'].split(" ")
+        ingredients = flask.request.form['ingredient'].split("\r\n")
 
-		# parse string correctly
+		# parse ingredients correctly
         meal_ingredients = {}
-        for i, word in enumerate(ingredients):
-			ingredient_key = ""
-			if word.isalpha():
-				ingredient_key = ingredient_key + word
-			else:
-				ingredient_value = word
-				meal_ingredients[str(ingredient_key)] = str(ingredient_value)
-
+        for i, line in enumerate(ingredients):
+            line = line.split(' ')
+            ingredient_key = ''
+            ingredient_value = ''
+            for word in line:
+                try:
+                    ingredient_value = float(word)
+                except ValueError:
+                    ingredient_key = ingredient_key+' '+word
+            if ingredient_key == '': pass
+            meal_ingredients[str(ingredient_key).strip()] = str(ingredient_value)
+        meal_ingredients.pop('', None)
         meal_object = Meal(name, meal_type, **meal_ingredients)
 
-        if flask.request.form['method']:
-           meal_object.Method(flask.request.form['method'])
-        instance.create(meal_object)
-        return flask.redirect("/database.html")
+        if flask.request.form['method']: meal_object.Method(flask.request.form['method'])
 
-    elif flask.request.method == 'DELETE':
-        "Delete all entries."
-        return flask.jsonify({'deleted':instance.deleteAll()})
+        meal_db.create(meal_object)
+        return flask.redirect("http://localhost:3001/database.html")
+
+    # elif flask.request.method == 'DELETE':
+    #     '''Delete all entries.'''
+    #     return flask.jsonify({'deleted':meal_db.deleteAll()})
 
     else:
         return {'error':'oops'}
 
 
 @app.route('/meals/keyword=<key>/value=<value>', methods = ['GET','PUT','DELETE'])
-def readOneEntries(key, value):
+def OneEntry(key, value):
 
     '''Entries that match kv pair in database.'''
 
     if flask.request.method == 'GET':
         "List entries."
-
-        return flask.jsonify({key+':'+value:instance.readByField({key:value})})
+        return flask.jsonify({key+':'+value:meal_db.readByField({key:value})})
 
     elif flask.request.method == 'PUT':
         "Update entries."
-        update_object = instance.readByField({key:value})
-        #NOT SURE IN HERE YET
-        return flask.jsonify({key+':'+value:instance.readByField({key:value},{update_object})})
+        update_object = meal_db.readByField({key:value})
+
+        flask.request.form['name'] = update_object.name
+
+        return flask.jsonify({key+':'+value:meal_db.readByField({key:value},{update_object})})
 
     elif flask.request.method == 'DELETE':
         "Delete entries."
-        return flask.jsonify({key+':'+value:instance.deleteByField({key:value})})
+        return flask.jsonify({key+':'+value:meal_db.deleteByField({key:value})})
 
     else:
         return {'error':'oops'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #------------- MealPlan Methods -------------#
@@ -102,20 +146,20 @@ def readOneEntries(key, value):
 @app.route('/meals/_id/<_id>/', methods = ['POST','DELETE'])
 def addMealToMealList(_id):
 
-    '''Add a meal_object to instance.meals.'''
+    '''Add a meal_object to mealplan_db.meals.'''
 
     if flask.request.method == "POST":
 
-        meal_object = instance.readByField({'_id':_id})
-        instance.addMeal(meal_object)
+        meal_object = mealplan_db.readByField({'_id':_id})
+        mealplan_db.addMeal(meal_object)
         return flask.jsonify({'meal added:': meal_object})
 
     elif flask.request.method == "DELETE":
 
-        '''Remove a meal_object from instance.meals.'''
+        '''Remove a meal_object from mealplan_db.meals.'''
 
-        meal_object = instance.readByField({'_id':_id})
-        instance.removeMeal(meal_object)
+        meal_object = mealplan_db.readByField({'_id':_id})
+        mealplan_db.removeMeal(meal_object)
         return flask.jsonify({'meal removed:': meal_object})
 
     else:
@@ -127,14 +171,14 @@ def addMealToMealList(_id):
 def nRandomMeals(n):
 
     '''N random meals from database.'''
-    return flask.jsonify({str(n)+' random meal(s):': instance.randomMeals(int(n))})
+    return flask.jsonify({str(n)+' random meal(s):': mealplan_db.randomMeals(int(n))})
 
 
 @app.route('/meals/random', methods = ['GET'])
 def fiveRandomMeals():
 
     '''Default 5 random meals from database.'''
-    return flask.jsonify({'5 random meals:': instance.randomMeals(5)})
+    return flask.jsonify({'5 random meals:': mealplan_db.randomMeals(5)})
 
 
 
